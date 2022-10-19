864f2bad3dc8b93f9ac4d225e779e8787b2a2eef
KamilaIwon <iwon.kamila@gmail.com>
2022-10-19 17:43:17 +0200

    lab4

--- lab3/treenode.py
+++ lab3/treenode.py
@@ -5,12 +5,9 @@ class TreeNode:
     value: Any
     children: List['TreeNode']
 
-    def __int__(self, value=Any):
+    def __init__(self, value=Any):
+        self.children = []
         self.value = value
-        self.children = None
-
-    def __str__(self):
-        return str(self.value)
 
     def is_leaf(self) -> bool:
         if self.children is None:
@@ -20,4 +17,14 @@ class TreeNode:
     def add(self, child: 'TreeNode') -> None:
         self.children.append(child)
 
+
+b = TreeNode(10)
+c = TreeNode(5)
+d = TreeNode(3)
+print(b.value)
+print(b.children)
+print(b.is_leaf())
+b.add(c)
+b.add(d)
+print(b.children)
 
